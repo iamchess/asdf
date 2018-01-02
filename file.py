6ebc573562cbdd1a7077a5e36350efa77926f657
imm3 <35004527+imm3@users.noreply.github.com>
2018-01-02 15:16:19 +1100

    Update file.py

--- file.py
+++ file.py
@@ -24,3 +24,73 @@ def mBishop:
 	y = 0
 	getPos = [x][y]
 	
+	
+def validMoves(x, y, piece):
+	validSpace = []
+	if piece == rook:
+		for a in range(x):
+			validSpace.append(str(a) + str(y))
+		for b in range(y):
+			validSpace.append(str(x) + str(b))
+	
+	elif piece == bishop:
+		for a in range(7):
+			if x + a <= 7 and y + a <= 7:
+				validSpace.append(str(x + a) + str(y + a))
+			
+			if x - a >= 0 and y - a >= 0:
+				validSpace.append(str(x - a) + str(y - a))
+					
+			if x - a >= 0 and y + a <= 7:
+				validSpace.append(str(x - a) + str(y + a))
+					
+			if x + a <= 7 and y - a >= 0:
+				validSpace.append(str(x + a) + str(y + a))
+	
+	elif piece == knight:
+		if x + 2 <= 7:
+			if y + 1 <= 7:
+				validSpace.append(str(x + 2) + str(y + 1))
+			if y - 1 >= 0:
+				validSpace.append(str(x + 2) + str(y - 1))
+		
+		if x - 2 >= 0:
+			if y + 1 <= 7:
+				validSpace.append(str(x - 2) + str(y + 1))
+			if y - 1 >= 0:
+				validSpace.append(str(x - 2) + str(y - 1))
+				
+		if y + 2 <= 7:
+			if x + 1 <= 7:
+				validSpace.append(str(x + 1) + str(y + 2))
+			if x - 1 >= 0:
+				validSpace.append(str(x - 1) + str(y + 2))
+				
+		if y - 2 >= 0:
+			if x + 1 <= 7:
+				validSpace.append(str(x + 1) + str(y - 2))
+			if x - 1 >= 0:
+				validSpace.append(str(x - 1) + str(y - 2))
+	
+	elif piece == queen:
+		for a in range(x):
+			validSpace.append(str(a) + str(y))
+			if x + a <= 7 and y + a <= 7:
+				validSpace.append(str(x + a) + str(y + a))
+			
+			if x - a >= 0 and y - a >= 0:
+				validSpace.append(str(x - a) + str(y - a))
+					
+			if x - a >= 0 and y + a <= 7:
+				validSpace.append(str(x - a) + str(y + a))
+					
+			if x + a <= 7 and y - a >= 0:
+				validSpace.append(str(x + a) + str(y + a))
+		for b in range(y):
+			validSpace.append(str(x) + str(b))
+	
+	elif piece == king:
+		asdf
+	
+	elif piece == pawn:
+		asdf
